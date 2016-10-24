a3af00783c915f6a6c4957d36985c27286fef289
Takahiro KITAHARA <fliedonion@users.noreply.github.com>
2016-10-24 09:58:05 +0900

    Initial Commit

---
+++ XElementParser/XElementDynamicParser.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Case_of_t.net.DynamicParsers {
+    public sealed class XElementDynamicParser {
+        private byte tryByteOut;
+        private int tryIntOut;
+        private long tryLongOut;
+        private float tryFloatOut;
+        private double tryDoubleOut;
+        private decimal tryDecimalOut;
+
+        public string ElementValueSeparator { get; set; }
+        public bool ElementValueAlwaysString { get; set; }
+
+        private bool SuppressAttrElePrefix {
+            get { return !AppendPrefixToAvoidNameConflictBetweenElementAndAttribute; }
+        }
+
+        /// <summary>
+        /// default <c>false</c>
+        /// </summary>
+        public bool AppendPrefixToAvoidNameConflictBetweenElementAndAttribute { get; set; }
+
+        [DefaultValue(false)] // for only indicate in xmlcomment document
+        public bool TryByteParse { get; set; }
+        [DefaultValue(true)] // for only indicate in xmlcomment document
+        public bool TryIntParse { get; set; }
+        [DefaultValue(false)] // for only indicate in xmlcomment document
+        public bool TryLongParse { get; set; }
+        [DefaultValue(false)] // for only indicate in xmlcomment document
+        public bool TryFloatParse { get; set; }
+        [DefaultValue(true)] // for only indicate in xmlcomment document
+        public bool TryDoubleParse { get; set; }
+        [DefaultValue(false)] // for only indicate in xmlcomment document
+        public bool TryDecimalParse { get; set; }
+
+        public void SetAllTryParseOptionOff() {
+            TryByteParse = false;
+            TryIntParse = false;
+            TryLongParse = false;
+            TryFloatParse = false;
+            TryDoubleParse = false;
+            TryDecimalParse = false;
+            
+        }
+        public void SetAllTryParseOptionOn() {
+            TryByteParse = true;
+            TryIntParse = true;
+            TryLongParse = true;
+            TryFloatParse = true;
+            TryDoubleParse = true;
+            TryDecimalParse = true;
+        }
+        public void SetAllTryParseOptionDefault() {
+            TryByteParse = false;
+            TryIntParse = true;
+            TryLongParse = false;
+            TryFloatParse = false;
+            TryDoubleParse = true;
+            TryDecimalParse = false;
+        }
+
+
+        public XElementDynamicParser(bool appendPrefixToAvoidNameConflictBetweenElementAndAttribute = false) {
+            SetPropertyInitialValues();
+            // overwrite constructor specified
+            AppendPrefixToAvoidNameConflictBetweenElementAndAttribute = appendPrefixToAvoidNameConflictBetweenElementAndAttribute;
+        }
+
+        private void SetPropertyInitialValues() {
+            AppendPrefixToAvoidNameConflictBetweenElementAndAttribute = false;
+            SetAllTryParseOptionDefault();
+            ElementValueSeparator = "";
+            ElementValueAlwaysString = false;
+        }
+
+
+        public dynamic Parse(XElement target) {
+            var item = new ExpandoObject();
+            if (target != null) {
+                Parse(item, target);
+                return item;
+            }
+            return null;
+        }
+
+
+        private void Parse(ExpandoObject item, XElement target) {
+            AddElementValueAndValuesAsProperties(item, target.Nodes().OfType<XText>());
+            if (target.HasElements) {
+                var namesWithCount = target.Elements()
+                    .GroupBy(x => x.Name)
+                    .Select(x => new {XName = x.Key, Count = x.Count()});
+
+                foreach (var element in namesWithCount) {
+                    if (element.Count == 1) {
+                        ParseSingleElement(item, target.Elements(element.XName).First());
+                    }
+                    else {
+                        ParseMultipleElement(item, target.Elements(element.XName));
+                    }
+                }
+            }
+            AddAttributesAsProperties(item, target.Attributes());    
+        }
+
+
+        private void ParseSingleElement(ExpandoObject parent, XElement target) {
+
+            var item = new ExpandoObject();
+            AddProperty(parent, (SuppressAttrElePrefix ? "" : "e") + target.Name.LocalName, item); // Add Element as property named element name.
+
+            AddElementValueAndValuesAsProperties(item, target.Nodes().OfType<XText>());
+            AddAttributesAsProperties(item, target.Attributes());
+            Parse(item, target);
+        }
+
+        private void ParseMultipleElement(ExpandoObject parent, IEnumerable<XElement> targets) {
+            var list = new List<ExpandoObject>();
+            AddProperty(parent, (SuppressAttrElePrefix ? "" : "e") + targets.First().Name.LocalName, list); // Add Element as property named element name.
+            foreach (var target in targets) {
+                var item = new ExpandoObject();
+                AddElementValueAndValuesAsProperties(item, target.Nodes().OfType<XText>());
+                AddAttributesAsProperties(item, target.Attributes());
+                Parse(item, target);
+            }
+        }
+
+        private void AddElementValueAndValuesAsProperties(IDictionary<string, object> that, IEnumerable<XText> texts) {
+
+            var values = texts.Select(x => x.Value).ToList();
+            var value = string.Join(ElementValueSeparator, values);
+
+            AddProperty(that, "Values", (object)values); // Add Element Texts as `Values` (List<string>) property
+            if (ElementValueAlwaysString) {
+                AddProperty(that, "Value", (object) value); // `Value` as string
+            }
+            else {
+                AddProperty(that, "Value", value); 
+            }
+        }
+
+
+        private void AddAttributesAsProperties(ExpandoObject item, IEnumerable<XAttribute> attrs) {
+            foreach (XAttribute x in attrs) {
+                var name = (SuppressAttrElePrefix ? "" : "p") + x.Name.LocalName;
+                AddProperty(item, name, x.Value);
+            }
+        }
+
+        private void AddProperty(IDictionary<string, object> that, string name, List<ExpandoObject> value) {
+            if (that.ContainsKey(name)) return;
+            that.Add(name, value);
+        }
+
+        private void AddProperty(IDictionary<string, object> that, string name, ExpandoObject value) {
+            if (that.ContainsKey(name)) return;
+            that.Add(name, value);
+        }
+
+        private void AddProperty(IDictionary<string, object> that, string name, string value) {
+            if (that.ContainsKey(name)) return;
+
+            if (TryByteParse && byte.TryParse(value, out tryByteOut)) {
+                that.Add(name, tryByteOut);
+                return;
+            }
+            if (TryIntParse && int.TryParse(value, out tryIntOut)) {
+                that.Add(name, tryIntOut);
+                return;
+            }
+            if (TryLongParse && long.TryParse(value, out tryLongOut)) {
+                that.Add(name, tryLongOut);
+                return;
+            }
+            if (TryFloatParse && float.TryParse(value, out tryFloatOut)) {
+                that.Add(name, tryFloatOut);
+                return;
+            }
+            if (TryDoubleParse && double.TryParse(value, out tryDoubleOut)) {
+                that.Add(name, tryDoubleOut);
+                return;
+            }
+            if (TryDecimalParse && decimal.TryParse(value, out tryDecimalOut)) {
+                that.Add(name, tryDecimalOut);
+                return;
+            }
+            that.Add(name, value);
+        }
+
+        private void AddProperty(IDictionary<string, object> that, string name, object value) {
+            if (that.ContainsKey(name)) return;
+            that.Add(name, value);
+        }
+    }
+
+}
---
+++ tests/XElementDynamicParserTest/XElementDynamicParserTests.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Xml.Linq;
+using Case_of_t.net.DynamicParsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Case_of_t.net.DynamicParsers.XElementDynamicParserTest {
+
+    [TestClass]
+    public class TestXElementDynamicParser {
+
+        [TestClass]
+        public class CanParse {
+            public TestContext TestContext { get; set; }
+            private XElementDynamicParser parser;
+
+            [TestInitialize]
+            public void BeforeEach() {
+                parser = new XElementDynamicParser();
+            }
+
+            [TestMethod]
+            [TestCase(@"<a>12</a>", 12)]
+            public void TopElementValue() {
+                TestContext.Run((string a, object b) => {
+                    dynamic x = parser.Parse(XElement.Parse(a));
+                    (x.Value as object).Is(b);
+                });
+            }
+
+            [TestMethod]
+            [TestCase(@"<a><![CDATA[abc]]></a>", "abc")]
+            [TestCase(@"<a><![CDATA[ab]]><![CDATA[c]]></a>", "abc")]
+            public void TopElementCdata() {
+                TestContext.Run((string a, object b) => {
+                    dynamic x = parser.Parse(XElement.Parse(a));
+                    (x.Value as object).Is(b);
+                });
+            }
+
+            [TestMethod]
+            [TestCase(@"<a test=""testattr""></a>", "testattr")]
+            public void TopElementAttribute() {
+                TestContext.Run((string src, object exp) => {
+                    dynamic x = parser.Parse(XElement.Parse(src));
+                    (x.test as object).Is(exp);
+                });
+            }
+
+            [TestMethod]
+            [TestCase(@"<a test=""testattr"">12<b><c> <cd><e><f attrF=""attrF"">F</f><f attrF=""attrF"">F</f></e></cd> <cA><B><C attrC=""attrC"">C</C></B></cA> </c></b></a>", "testattr", 12, "attrF", "F", "attrC", "C")]
+            public void DeepElement() {
+                TestContext.Run((string src, object av, object ev, object afv, object efv, object aCv, object eCv) => {
+                    dynamic x = parser.Parse(XElement.Parse(src));
+                    (x.test as object).Is(av);
+                    (x.Value as object).Is(ev);
+                    foreach (var f in x.b.c.cd.e.f) {
+                        f.attrF.Is(afv);
+                        f.Value.Is(efv);
+                    }
+                    (x.b.c.cA.B.C.attrC as object).Is(aCv);
+                    (x.b.c.cA.B.C.Value as object).Is(eCv);
+                });
+            }
+
+
+            [TestMethod]
+            [TestCase(@"<a><b>1</b><b>2</b><b>3</b><b>4</b></a>", new object[]{1,2,3,4})]
+            public void MultiElementsValue() {
+                TestContext.Run((string src, object[] expects) => {
+                    dynamic x = parser.Parse(XElement.Parse(src));
+                    var expectIndex = 0;
+
+                    foreach (var ele in x.b) {
+                        ele.Value.Is(expects[expectIndex]);
+                        expectIndex++;
+                    }
+                });
+            }
+
+            [TestMethod]
+            [TestCase(@"<a><b attr=""1""></b><b attr=""2""></b><b attr=""3""></b><b attr=""4""></b></a>", new object[] { 1, 2, 3, 4 })]
+            public void MultiElementAttributes() {
+                TestContext.Run((string src, object[] expects) => {
+                    dynamic x = parser.Parse(XElement.Parse(src));
+                    var expectIndex = 0;
+
+                    foreach (var ele in x.b) {
+                        ele.attr.Value.Is(expects[expectIndex]);
+                        expectIndex++;
+                    }
+                });
+            }
+            
+
+            [TestMethod]
+            [TestCase(@"<a><b>12</b></a>", 12)]
+            [TestCase(@"<a><b>1.2</b></a>", 1.2)]
+            [TestCase(@"<a><b>1.213131</b></a>", 1.213131)]
+            [TestCase(@"<a><b>1.2.</b></a>", "1.2.")]
+            public void ValueWithDefault() {
+                TestContext.Run((string a, object b) => {
+                    var x = parser.Parse(XElement.Parse(a));
+                    (x.b.Value as object).Is(b);
+                });
+            }
+
+
+
+
+            [TestMethod]
+            [TestCaseSource("DecimalIfCouldSource")]
+            public void DecimalIfCould() {
+                parser.SetAllTryParseOptionOff();
+                parser.TryDecimalParse = true;
+
+                TestContext.Run((string src, object exp) => {
+                    dynamic x = parser.Parse(XElement.Parse(src));
+                    (x.b.Value as object).Is(exp);
+                    (x.b.at as object).Is(exp);
+                });
+            }
+
+            public static object[] DecimalIfCouldSource = {
+                new object[] {@"<a><b at=""12"">12</b></a>", 12m},
+                new object[] {@"<a><b at=""1.2"">1.2</b></a>", 1.2m},
+                new object[] {@"<a><b at=""1.2."">1.2.</b></a>", "1.2."},
+            };
+
+
+
+            [TestMethod]
+            public void MultiAttributes() {
+                var xml = XElement.Parse("<a><b attra=\"1\" attrb=\"abc\" attrc=\"1.5\"></b></a>");
+                dynamic x = parser.Parse(xml);
+
+                (x.b.attra as object).Is(1);
+                (x.b.attrb as object).Is("abc");
+                (x.b.attrc as object).Is(1.5);
+            }
+
+
+
+            [TestMethod]
+            [TestCase("<a><b>1<a>12</a>2</b></a>", new object[] { 12, "1", "2"})]
+            [TestCase("<a><b>1<a>12</a>\n2</b></a>", new object[] { "1\n2", "1", "\n2" })]
+            [TestCase("<a><b>1\n<a>12</a>2</b></a>", new object[] { "1\n2", "1\n", "2" })]
+            [TestCase("<a><b>1<a>12</a>\r\n2</b></a>", new object[] { "1\n2", "1", "\n2" })]
+            [TestCase("<a><b>1\r\n<a>12</a>2</b></a>", new object[] { "1\n2", "1\n", "2" })]
+            [TestCase("<a><b><![CDATA[1]]><a>12</a><![CDATA[2]]></b></a>", new object[] { 12, "1", "2" })]
+            public void ElementValues() {
+
+                TestContext.Run((string src, object[] exp)=>{
+                    dynamic x = parser.Parse(XElement.Parse(src));
+                    (x.b.Value as object).Is(exp[0]);
+                    (x.b.Values[0] as object).Is(exp[1]);
+                    (x.b.Values[1] as object).Is(exp[2]);
+                });
+            }
+
+            [TestMethod]
+            [TestCase("<a><b>1<a>12</a>2</b></a>", new object[] { "1 2", "1", "2" })]
+            [TestCase("<a><b>1<a>12</a>\n2</b></a>", new object[] { "1 \n2", "1", "\n2" })]
+            [TestCase("<a><b>1\n<a>12</a>2</b></a>", new object[] { "1\n 2", "1\n", "2" })]
+            public void ElementValueSeparatorSeparatesValue() {
+
+                parser.ElementValueSeparator = " ";
+
+                TestContext.Run((string src, object[] exp) => {
+                    dynamic x = parser.Parse(XElement.Parse(src));
+                    (x.b.Value as object).Is(exp[0]);
+                    (x.b.Values[0] as object).Is(exp[1]);
+                    (x.b.Values[1] as object).Is(exp[2]);
+                });
+
+            }
+
+            [TestMethod]
+            [TestCase(@"<a-b><b>12</b></a-b>", 12)]
+            public void NameWithUnusableCharForCSharpVariable() {
+                TestContext.Run((string a, object b) => {
+                    var x = parser.Parse(XElement.Parse(a));
+                    (x.b.Value as object).Is(b);
+                });
+            }
+
+            [TestMethod]
+            [TestCase(@"<a-b><b.x data.attr=""x"">12</b.x></a-b>", "b.x", "data.attr", 12, "x")]
+            [TestCase(@"<a-b><b-x data-attr=""x"">12</b-x></a-b>", "b-x", "data-attr", 12, "x")]
+            public void NameWithUnusableCharForCSharpVariableCanAccessThroughIDictionary() {
+                TestContext.Run((string xml, string ename, string aname, object expE, object expA) => {
+                    var x = parser.Parse(XElement.Parse(xml)) as IDictionary<string, object>;
+                    (((dynamic)(x[ename])).Value as object).Is(expE);
+                    ((IDictionary<string, object>)x[ename])[aname].Is(expA);
+
+                });
+            }
+
+        }
+
+        [TestClass]
+        public class TryParsePriority {
+            public TestContext TestContext { get; set; }
+            private XElementDynamicParser parser;
+            [TestInitialize]
+            public void BeforeEach() {
+                parser = new XElementDynamicParser();
+                parser.SetAllTryParseOptionOn();
+            }
+
+            [TestMethod]
+            [TestCase(@"<a><b at=""1.2"">1.2</b></a>", 1.2f)]
+            [TestCase(@"<a><b at=""1.213131"">1.213131</b></a>", 1.213131f)]
+            [TestCase(@"<a><b at=""0.666666666666666"">0.666666666666666</b></a>", 0.666666666666666f)]
+            [TestCase(@"<a><b at=""1.2."">1.2.</b></a>", "1.2.")]
+            public void FloatHaveHigherPriorityThanDouble() {
+                
+                TestContext.Run((string src, object exp) => {
+                    dynamic x = parser.Parse(XElement.Parse(src));
+                    (x.b.Value as object).Is(exp);
+                    (x.b.at as object).Is(exp);
+                });
+            }
+
+            [TestMethod]
+            [TestCase(@"<a><b at=""12"">12</b></a>", (byte)12)]
+            [TestCase(@"<a><b at=""255"">255</b></a>", (byte)255)]
+            [TestCase(@"<a><b at=""256"">256</b></a>", 256)]
+            public void ByteHaveHigherPriorityThanInt() {
+                TestContext.Run((string src, object exp) => {
+                    dynamic x = parser.Parse(XElement.Parse(src));
+                    (x.b.Value as object).Is(exp);
+                    (x.b.at as object).Is(exp);
+                });
+            }
+        }
+
+        [TestClass]
+        public class ResolveNameConflictsWhenSuppressPrefix {
+            public TestContext TestContext { get; set; }
+            private XElementDynamicParser parser;
+
+            [TestInitialize]
+            public void BeforeEach() {
+                parser = new XElementDynamicParser();
+            }
+
+            [TestMethod]
+            public void AttributePriorThanSameNameChildElement() {
+                var xml = XElement.Parse("<a><b a=\"1\"><a>12</a></b></a>");
+                dynamic x = parser.Parse(xml);
+                (x.b.a as object).Is(1);
+            }
+
+
+            [TestMethod]
+            public void ElementValuePriorThanAttributeAndChildThatHaveSameName() {
+                var xml = XElement.Parse("<a><b Value=\"abdefg\">PROIR<Value>xyz</Value></b></a>");
+                dynamic x = parser.Parse(xml);
+                (x.b.Value as object).Is("PROIR");
+            }
+        }
+
+        [TestClass]
+        public class ResolveNameConflictsWithPrefix {
+            public TestContext TestContext { get; set; }
+            private XElementDynamicParser parser;
+
+            [TestInitialize]
+            public void BeforeEach() {
+                parser = new XElementDynamicParser();
+                parser.AppendPrefixToAvoidNameConflictBetweenElementAndAttribute = true;
+            }
+
+            [TestMethod]
+            public void EachItemsHavePrefix() {
+                var xml = XElement.Parse("<a><b Value=\"abdefg\">opq<Value>xyz</Value>RST</b></a>");
+                dynamic x = parser.Parse(xml);
+                (x.eb.Value as object).Is("opqRST", "Element Value");
+                (x.eb.pValue as object).Is("abdefg", "Attribute(prop's p)");
+                (x.eb.eValue.Value as object).Is("xyz", "ChildElement Value");
+            }
+        }
+
+    }
+
+}
